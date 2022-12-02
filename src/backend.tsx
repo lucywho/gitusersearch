5e4f17b3bb46a519c4d0c517c4f64c536dd1206e
Lucy Toman <lucy@toman.me.uk>
2022-12-02 16:30:39 +0100

    cleans up created_at string to display date

--- src/backend.tsx
+++ src/backend.tsx
@@ -1,20 +1,26 @@
 import { GitHubUser } from "./data-type"
 import { fetchGitHubData } from "./networkClient"
 
+function cleanUpDate(created: string) {
+    let date = new Date(created)
+    let month = date.toLocaleDateString("default", { month: "short" })
+    return `${date.getDate()} ${month} ${date.getFullYear()}`
+}
+
 export async function fetchData(userInput: string) {
     const json = await fetchGitHubData(userInput)
-
-    //TODO: clean up date string
 
     if (!json) {
         return
     }
 
+    const cleanDate = cleanUpDate(json.created_at)
+
     const newUser: GitHubUser = {
         name: json.name,
         login: json.login,
         avatar_url: json.avatar_url,
-        created_at: json.created_at,
+        created_at: cleanDate,
         bio: json.bio,
         public_repos: json.public_repos,
         followers: json.followers,
